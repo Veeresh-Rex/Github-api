5c5029d1d1ef3c42c0b66bf7c3ed94686a5add1c
Veeresh Rex <Veeresh-Rex@users.noreply.github.com>
2021-03-17 16:11:50 +0530

    added list of repository container and model

---
+++ script.js
@@ -0,0 +1,28 @@
+const btnSearch = document.querySelector('#searchBtn');
+const input = document.querySelector('#usernameInp');
+const imageUser = document.querySelectorAll('.imageUser')[0];
+console.log(imageUser);
+console.log(btnSearch);
+var userData;
+btnSearch.addEventListener('click', () => {
+  console.log('click');
+  console.log(input.value);
+  let url = `https://api.github.com/users/${input.value}`;
+  console.log(url);
+  fetch(url)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      userData = data;
+      imageUser.insertAdjacentHTML(
+        'beforebegin',
+        `<img src="https://avatars.githubusercontent.com/u/52687597?v=4" style="width: 220px;height:fit-content;" alt="">`
+      );
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  input.value = '';
+});
